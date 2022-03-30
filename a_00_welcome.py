d35d6376621a1c8253f5a76400eda8d7807ce9af
Caviuna <abaquar.dev@gmail.com>
2022-03-29 23:50:23 -0300

    up first code

---
+++ a_00_welcome.py
@@ -0,0 +1,13 @@
+""" My first program."""
+
+class HelloWorld:
+    """Class HelloWorld:"""
+    def __init__(self):
+        """Constructor"""
+        self.message = "Hello World! \n Programar é preciso, viver não é preciso."
+    def print_message(self):
+        """Print message"""
+        print(self.message)
+
+python_learnig = HelloWorld()
+python_learnig.print_message()
